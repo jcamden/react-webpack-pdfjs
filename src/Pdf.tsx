c6068c23a661f9d872295d854db77a52e80637a7
John Camden <jcamden@box.com>
2021-12-01 17:06:43 -0500

    basic ts, react, webpack, etc

---
+++ src/Pdf.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export interface PdfProps {}
+
+export const Pdf: React.FC<PdfProps> = ({}) => {
+  return <div>Meow!</div>;
+};
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Pdf } from "./Pdf";
+
+ReactDOM.render(<Pdf />, document.getElementById("app"));
